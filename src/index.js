a4a6331d6575534f71ba4ef45713b1a430020ed4
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-17 17:47:08 +0600

    try to solve prolem

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5174",
+    origin:["http://localhost:5173","dapper-froyo-c36250.netlify.app"],
     credentials:true
 }))
 
